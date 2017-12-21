f778a8223ad6af5ba94303ac5d0cc5db1f0403ad
Artyom Mironov <kazufukurou@gmail.com>
2017-12-21 23:28:08 +0300

    fix custom keymap

--- custom/my/keymaps/default/keymap.c
+++ custom/my/keymaps/default/keymap.c
@@ -3,7 +3,9 @@
 #define SFT_ENT SFT_T(KC_ENT)
 #define SFT_BS SFT_T(KC_BSPC)
 #define CTL_QUO CTL_T(KC_QUOT)
-#define ALT_TAB ALT_T(KC_TAB)
+#define ALT_ESC ALT_T(KC_ESC)
+#define MO1_SPC LT(1, KC_SPC)
+#define MO2_TAB LT(2, KC_TAB)
 #define PREVENT_STUCK_MODIFIERS
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -16,17 +18,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
  * |Ctl/'" |   Q   |   J   |   K   |   X   |   B   |   M   |   W   |   V   |   Z   |
  * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
- * |       |Tab/Alt|  Fn1  |Shf/Bs | Space | Space |Sft/Ent|  Fn2  |  -_   |       |
+ * |       |Alt/Esc|MO1/Spc|Shf/Bs | Space | Space |Sft/Ent|MO2/Tab|  -_   |       |
  * '-------------------------------------------------------------------------------'
  */
     [0] = {
-        { KC_SCLN, KC_COM,   KC_DOT, KC_P,    KC_Y,   KC_F,   KC_G,    KC_C,  KC_R,    KC_L  },
-        { KC_A,    KC_O,     KC_E,   KC_U,    KC_I,   KC_D,   KC_H,    KC_T,  KC_N,    KC_S  },
-        { CTL_QUO, KC_Q,     KC_J,   KC_K,    KC_X,   KC_B,   KC_M,    KC_W,  KC_V,    KC_Z  },
-        { KC_NO,   ALT_TAB,  MO(1),  SFT_BS,  KC_SPC, KC_SPC, SFT_ENT, MO(2), KC_MINS, KC_NO }
+        { KC_SCLN, KC_COMM,  KC_DOT,  KC_P,    KC_Y,   KC_F,   KC_G,    KC_C,    KC_R,    KC_L  },
+        { KC_A,    KC_O,     KC_E,    KC_U,    KC_I,   KC_D,   KC_H,    KC_T,    KC_N,    KC_S  },
+        { CTL_QUO, KC_Q,     KC_J,    KC_K,    KC_X,   KC_B,   KC_M,    KC_W,    KC_V,    KC_Z  },
+        { KC_NO,   ALT_ESC,  MO1_SPC, SFT_BS,  KC_SPC, KC_SPC, SFT_ENT, MO2_TAB, KC_MINS, KC_NO }
     },
 /*
- * Fn1
+ * Layer 1
  * .-------------------------------------------------------------------------------.
  * |   ~   |   %   |   {   |   }   |   ^   |   =   |   7   |   8   |   9   |   !   |
  * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
@@ -34,17 +36,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
  * |   \   |   `   |   [   |   ]   |   #   |   +   |   1   |   2   |   3   |   /   |
  * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
- * |       |  Esc  |  Fn1  |       |       |       |       |   0   |   |   |       |
+ * |       |       |  MO1  |       |       |       |       |   0   |   |   |       |
  * '-------------------------------------------------------------------------------'
  */
     [1] = {
         { KC_TILD, KC_PERC, KC_LCBR, KC_RCBR, KC_CIRC, KC_EQL,  KC_7,  KC_8,    KC_9,    KC_EXLM },
         { KC_DLR,  KC_AMPR, KC_LPRN, KC_RPRN, KC_AT,   KC_ASTR, KC_4,  KC_5,    KC_6,    KC_QUES },
         { KC_BSLS, KC_GRV,  KC_LBRC, KC_RBRC, KC_HASH, KC_PLUS, KC_1,  KC_2,    KC_3,    KC_SLSH },
-        { KC_NO,   KC_ESC,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_0,  KC_PIPE, KC_TRNS, KC_NO   }
+        { KC_NO,   KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_0,  KC_PIPE, KC_TRNS, KC_NO   }
     },
 /*
- * Fn2
+ * Layer 2
  * .-------------------------------------------------------------------------------.
  * |  F9   |  F10  |  F11  |  F12  |  MB1  | Home  |  End  |  Up   |  Ins  |  Del  |
  * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
@@ -52,17 +54,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
  * |  F1   |  F2   |  F3   |  F4   |  MB3  | PgDn  | Prev  | Play  | Next  | Vol-  |
  * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
- * |       |       |       |       |       |       | Reset |  Fn2  | Mute  |       |
+ * |       |       |       |       |       |       | Reset |  MO2  | Mute  |       |
  * '-------------------------------------------------------------------------------'
  */
-
     [2] = {
         {KC_F9, KC_F10, KC_F11,  KC_F12, KC_BTN1, KC_HOME, KC_END,  KC_UP,   KC_INS,  KC_DEL  },
         {KC_F5, KC_F6,  KC_F7,   KC_F8,  KC_BTN2, KC_PGUP, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLU },
         {KC_F1, KC_F2,  KC_F3,   KC_F4,  KC_BTN3, KC_PGDN, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD },
         {KC_NO, KC_NO,  KC_TRNS, KC_NO,  KC_NO,   KC_NO,   RESET,   KC_TRNS, KC_MUTE, KC_NO   }
     }
-
 };
 
 const uint16_t PROGMEM fn_actions[] = {
