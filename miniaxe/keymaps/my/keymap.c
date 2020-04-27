262803642bd5967d6df3c145d9a215a9b15c4671
Artyom Mironov <kazufukurou@gmail.com>
2020-04-28 04:19:53 +0900

    fix stuck shift key, move reset button

--- miniaxe/keymaps/my/keymap.c
+++ miniaxe/keymaps/my/keymap.c
@@ -42,28 +42,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_SCLN, KC_COMM, KC_DOT,  KC_P,    KC_Y,         KC_F,    KC_G,    KC_C,    KC_R,    KC_L, \
   KC_A,    KC_O,    KC_E,    KC_U,    KC_I,         KC_D,    KC_H,    KC_T,    KC_N,    KC_S, \
   KC_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,         KC_B,    KC_M,    KC_W,    KC_V,    KC_Z, \
-                    ALT_ESC, MO1_TAB, SFT_SPC,      SFT_ENT, MO2_BS,  CTL_MIN  \
-),
-
-[_RAISE] = LAYOUT( \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_BTN1,      KC_HOME, KC_END,  KC_UP,   KC_INS,  KC_DEL, \
-  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_BTN2,      KC_PGUP, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLU, \
-  KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BTN3,      KC_PGDN, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, \
-                    _______, _______, _______,      RESET,   _______, KC_MUTE \
+                    ALT_ESC, MO1_TAB, SFT_SPC,      SFT_ENT, MO2_BS,  CTL_MIN \
 ),
 
 [_LOWER] = LAYOUT( \
   KC_TILD, KC_PERC, KC_LCBR, KC_RCBR, KC_CIRC,      KC_EQL,  KC_7,    KC_8,    KC_9,    KC_EXLM, \
   KC_DLR,  KC_AMPR, KC_LPRN, KC_RPRN, KC_AT,        KC_ASTR, KC_4,    KC_5,    KC_6,    KC_QUES, \
   KC_BSLS, KC_GRV,  KC_LBRC, KC_RBRC, KC_HASH,      KC_PLUS, KC_1,    KC_2,    KC_3,    KC_SLSH, \
-                    _______, _______, _______,      TO(_ADJUST), KC_0,    KC_PIPE \
+                    RESET,   _______, KC_SPC,       _______, KC_0,    KC_PIPE \
 ),
 
-[_ADJUST] =  LAYOUT( \
+[_RAISE] = LAYOUT( \
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_BTN1,      KC_HOME, KC_END,  KC_UP,   KC_INS,  KC_DEL, \
+  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_BTN2,      KC_PGUP, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLU, \
+  KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BTN3,      KC_PGDN, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, \
+                    _______, _______, _______,      KC_MUTE, _______, TO(_ADJUST) \
+),
+
+[_ADJUST] = LAYOUT( \
   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,         KC_T,    KC_Y,    KC_U,    KC_I,    KC_O, \
   KC_TAB,  KC_A,    KC_S,    KC_D,    KC_D,         KC_G,    KC_H,    KC_J,    KC_K,    KC_L, \
   KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,         KC_B,    KC_N,    KC_M,    KC_P,    KC_PSCR, \
-                    KC_LALT, KC_LSFT, KC_SPC,       KC_ENT,  KC_BSPC, TO(_QWERTY)  \
+                    KC_LALT, KC_LSFT, KC_SPC,       KC_ENT,  KC_BSPC, TO(_QWERTY) \
 )
 };
 
