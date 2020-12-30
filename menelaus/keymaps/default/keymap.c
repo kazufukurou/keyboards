3eea0537024aae68942078f88f4f453b04acb21b
Artyom Mironov <kazufukurou@gmail.com>
2020-12-30 15:53:04 +0900

    fix f keys

--- menelaus/keymaps/default/keymap.c
+++ menelaus/keymaps/default/keymap.c
@@ -24,9 +24,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         { ___X___, ___X___, RESET,   _______, KC_SPC,        _______, KC_0,    KC_PIPE, ___X___, ___X___ }
     },
     [2] = {
-        { KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_BTN1,       KC_HOME, KC_END,  KC_UP,   KC_INS,  KC_DEL  },
+        { KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BTN1,       KC_HOME, KC_END,  KC_UP,   KC_INS,  KC_DEL  },
         { KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_BTN2,       KC_PGUP, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLU },
-        { KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BTN3,       KC_PGDN, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD },
+        { KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_BTN3,       KC_PGDN, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD },
         { ___X___, ___X___, _______, _______, _______,       KC_MUTE, _______, TO(3),   ___X___, ___X___ }
     },
     [3] = {
--- miniaxe/keymaps/my/keymap.c
+++ miniaxe/keymaps/my/keymap.c
@@ -16,10 +16,10 @@
 #include QMK_KEYBOARD_H
 
 // Defines the keycodes used by our macros in process_record_user
-#define _QWERTY 0
+#define _BASE 0
 #define _LOWER 1
 #define _RAISE 2
-#define _ADJUST 16
+#define _QWERTY 16
 
 #define SFT_SPC SFT_T(KC_SPC)
 #define SFT_ENT SFT_T(KC_ENT)
@@ -38,7 +38,7 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[_QWERTY] = LAYOUT( \
+[_BASE] = LAYOUT( \
   KC_SCLN, KC_COMM, KC_DOT,  KC_P,    KC_Y,         KC_F,    KC_G,    KC_C,    KC_R,    KC_L, \
   KC_A,    KC_O,    KC_E,    KC_U,    KC_I,         KC_D,    KC_H,    KC_T,    KC_N,    KC_S, \
   KC_QUOT, KC_Q,    KC_J,    KC_K,    KC_X,         KC_B,    KC_M,    KC_W,    KC_V,    KC_Z, \
@@ -53,17 +53,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_RAISE] = LAYOUT( \
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_BTN1,      KC_HOME, KC_END,  KC_UP,   KC_INS,  KC_DEL, \
+  KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BTN1,      KC_HOME, KC_END,  KC_UP,   KC_INS,  KC_DEL, \
   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_BTN2,      KC_PGUP, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLU, \
-  KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BTN3,      KC_PGDN, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, \
-                    _______, _______, _______,      KC_MUTE, _______, TO(_ADJUST) \
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_BTN3,      KC_PGDN, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLD, \
+                    _______, _______, _______,      KC_MUTE, _______, TO(_QWERTY) \
 ),
 
-[_ADJUST] = LAYOUT( \
+[_QWERTY] = LAYOUT( \
   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,         KC_T,    KC_Y,    KC_U,    KC_I,    KC_O, \
   KC_TAB,  KC_A,    KC_S,    KC_D,    KC_D,         KC_G,    KC_H,    KC_J,    KC_K,    KC_L, \
   KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,         KC_B,    KC_N,    KC_M,    KC_P,    KC_PSCR, \
-                    KC_LALT, KC_LSFT, KC_SPC,       KC_ENT,  KC_BSPC, TO(_QWERTY) \
+                    KC_LALT, KC_LSFT, KC_SPC,       KC_ENT,  KC_BSPC, TO(_BASE) \
 )
 };
 
@@ -71,36 +71,36 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case QWERTY:
       if (record->event.pressed) {
-        // persistant_default_layer_set(1UL<<_QWERTY);
-        set_single_persistent_default_layer(_QWERTY);
+        // persistant_default_layer_set(1UL<<_BASE);
+        set_single_persistent_default_layer(_BASE);
       }
       return false;
       break;
     case LOWER:
       if (record->event.pressed) {
         layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        update_tri_layer(_LOWER, _RAISE, _QWERTY);
       } else {
         layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        update_tri_layer(_LOWER, _RAISE, _QWERTY);
       }
       return false;
       break;
     case RAISE:
       if (record->event.pressed) {
         layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        update_tri_layer(_LOWER, _RAISE, _QWERTY);
       } else {
         layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        update_tri_layer(_LOWER, _RAISE, _QWERTY);
       }
       return false;
       break;
     case ADJUST:
       if (record->event.pressed) {
-        layer_on(_ADJUST);
+        layer_on(_QWERTY);
       } else {
-        layer_off(_ADJUST);
+        layer_off(_QWERTY);
       }
       return false;
       break;
